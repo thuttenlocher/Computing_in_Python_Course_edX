e15ca02c1299e7b5042513086a5e2d53fc3fbfd2
TylerHoot <huttenlochert@gmail.com>
2020-05-26 11:28:14 -0500

    Create 4.5.2 Coding Exercise 2.py

---
+++ 4.5.2 Coding Exercise 2.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 14:13:00 2020
+
+@author: Tyler H
+"""
+
+#Create a function called tup_to_dict. tup_to_dict should take one
+#parameter: a list of tuples. You can assume each tuple in
+#the list has exactly two values.
+#
+#The function should return a dictionary where the first item
+#in each tuple is the key, and the second item in each tuple
+#is the corresponding value.
+#
+#For example:
+# colors = [("turquoise", "#40E0D0"), ("red", "#990000")]
+# tup_to_dict(colors) -> {"turquoise":"#40E0D0", "red":"#990000"}
+#
+#Hint: the previous exercise is very similar; this just turns
+#it into a function! All our tuples will be color name-color
+#value pairs.
+
+
+#Write your function here!
+def tup_to_dict(data):
+    data_dict = {}
+    for x in data:
+        (color, code) = x
+        data_dict[color] = code
+    return data_dict
+
+
+#Below are some lines of code that will test your function.
+#You can change the value of the variable(s) to test your
+#function with different inputs.
+#
+#If your function works correctly, this will originally
+#print:  {'Turquoise':'#40E0D0', 'Red':'#990000'}
+#
+#Don't worry if it prints those in the reverse order; that's
+#still correct!
+print(tup_to_dict([("Turquoise", "#40E0D0"), ("Red", "#990000")]))
+
+
